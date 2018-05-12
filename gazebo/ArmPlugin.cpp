abd771b6f5040b5ec1a9a43436813bd9410b481a
azhurb <zhurbitsky@gmail.com>
2018-05-12 23:06:51 +0000

    Add subsctibers

--- gazebo/ArmPlugin.cpp
+++ gazebo/ArmPlugin.cpp
@@ -139,6 +139,7 @@ void ArmPlugin::Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
 	*/
 	
 	//cameraSub = None;
+	cameraSub = cameraNode->Subscribe("/gazebo/arm_world/camera/link/camera/image", &ArmPlugin::onCameraMsg, this);
 
 	// Create our node for collision detection
 	collisionNode->Init();
@@ -149,6 +150,7 @@ void ArmPlugin::Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
 	*/
 	
 	//collisionSub = None;
+	collisionSub = cameraNode->Subscribe("/gazebo/arm_world/tube/tube_link/my_contact", &ArmPlugin::onCollisionMsg, this);
 
 	// Listen to the update event. This event is broadcast every simulation iteration.
 	this->updateConnection = event::Events::ConnectWorldUpdateBegin(boost::bind(&ArmPlugin::OnUpdate, this, _1));
@@ -167,7 +169,11 @@ bool ArmPlugin::createAgent()
 	/
 	*/
 	
-	agent = NULL;
+	agent = dqnAgent::Create(INPUT_WIDTH, INPUT_HEIGHT, INPUT_CHANNELS, DOF * 2, 
+			OPTIMIZER, LEARNING_RATE, REPLAY_MEMORY, BATCH_SIZE,
+			GAMMA, EPS_START, EPS_END, EPS_DECAY,
+			USE_LSTM, LSTM_SIZE, ALLOW_RANDOM, DEBUG_DQN);
+	//agent = NULL;
 
 	if( !agent )
 	{
