bcf5e96a5671e402266a0f0ede83f0dc7b8242a6
alex <zhurbitsky@gmail.com>
2018-05-13 15:45:54 -0700

    Task 2 done

--- gazebo/ArmPlugin.cpp
+++ gazebo/ArmPlugin.cpp
@@ -38,8 +38,8 @@
 #define INPUT_WIDTH   64
 #define INPUT_HEIGHT  64
 #define OPTIMIZER "Adam"
-#define LEARNING_RATE 0.1f
-#define REPLAY_MEMORY 1000
+#define LEARNING_RATE 0.01f
+#define REPLAY_MEMORY 10000
 #define BATCH_SIZE 256
 #define USE_LSTM true
 #define LSTM_SIZE 256
@@ -265,10 +265,29 @@ void ArmPlugin::onCollisionMsg(ConstContactsPtr &contacts)
 		/ TODO - Check if there is collision between the arm and object, then issue learning reward
 		/
 		*/
-		
+
+		/* Task 1
 		if (strcmp(contacts->contact(i).collision1().c_str(), COLLISION_ITEM) == 0)
         {
 			rewardHistory = REWARD_WIN * 10;
+			newReward  = true;
+			endEpisode = true;
+
+			return;
+		}*/
+
+        // Task 2
+		if (strcmp(contacts->contact(i).collision1().c_str(), COLLISION_ITEM) == 0)
+		{
+			if (strcmp(contacts->contact(i).collision2().c_str(), COLLISION_POINT) == 0)
+			{
+				rewardHistory = REWARD_WIN * 20;
+			}
+			else
+			{
+				rewardHistory = REWARD_LOSS * 5;
+			}
+
 			newReward  = true;
 			endEpisode = true;
 
